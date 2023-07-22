4dd22c32d179c55fa8cb2d94a65db66ab22c1228
Wei Ji <23487320+weiji14@users.noreply.github.com>
2023-07-22 16:00:49 +1200

    :white_check_mark: Add unit test to check bambooflow version
    Must be greater than zero, but why?

---
+++ bambooflow/__init__.py
@@ -0,0 +1,7 @@
+"""
+bambooflow - First make the hollow pipe, then let the water flow~
+"""
+
+from importlib.metadata import version
+
+__version__ = version("bambooflow")  # e.g. 0.1.2.dev3+g0ab3cd78
