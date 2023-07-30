a547c936ad344b3c3342c87a02ec3b3cc32e2ea2
Wei Ji <23487320+weiji14@users.noreply.github.com>
2023-07-31 10:14:52 +1200

    :sparkles: AsyncIterDataPipe for running concurrent tasks (#5)
    * :sparkles: AsyncIterDataPipe for running concurrent tasks
    
    An asynchronous iterable-style DataPipe for processing tasks concurrently! Composition over inheritance. Subclassing from collections.abc.AsyncIterable in Python's standard library. Added some basic API docstring, and have setup some extlinks and intersphinx mappings in the docs/_config.yml file for linking to terms in the Python glossary.
    
    * :white_check_mark: Unit test for __repr__ method of AsyncIterDataPipe
    
    Minimal unit test to ensure that an AsyncIterDataPipe's qualified name is returned when calling repr() on it.
    
    * :memo: The big why of using bambooflow
    
    On where bambooflow fits in the world in relation to other libraries.

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # bambooflow
 
 First make the hollow pipe, then let the water flow~
+
+## Why bambooflow?
+
+🐍 Pure Python core and framework-agnostic, like [dataflow](https://github.com/tensorpack/dataflow)<br>
+🤹 Asynchronous and concurrency first, akin to [pypeln](https://github.com/cgarciae/pypeln)<br>
+⛓️ Composable dot-chain functional syntax, from [torchdata](https://github.com/pytorch/data)<br>
+🎍 Together, multi-tasking cooperatively, this is bambooflow!
---
+++ bambooflow/datapipes/aiter.py
@@ -0,0 +1,23 @@
+"""
+Base classes for Asynchronous Iterable DataPipes.
+"""
+import collections
+
+
+class AsyncIterDataPipe(collections.abc.AsyncIterable):
+    """
+    Asynchronous iterable-style DataPipes.
+
+    All DataPipes that represent an asynchronous iterable of data samples
+    should subclass this. This style of DataPipes is particularly useful for
+    performing I/O-bound tasks such as streaming data from a network disk drive
+    or reading multiple files concurrently. ``AsyncIterDataPipe`` is
+    initialized in a lazy fashion, and its elements are computed only when
+    :py:meth:`__anext__ <object.__anext__>` is called on the async iterator of
+    an ``AsyncIterDataPipe``.
+    """
+
+    def __repr__(self) -> str:
+        # Instead of showing <bamboopipe. ... .AsyncIterableWrapper at 0x.....>,
+        # return the qualified name of the class like <AsyncIterableWrapper>
+        return str(self.__class__.__qualname__)
