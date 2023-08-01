7e5dcc19953ca2f3009b1c10c55104df530ca840
Wei Ji <23487320+weiji14@users.noreply.github.com>
2023-08-01 17:08:42 +1200

    :sparkles: MapperAsyncIterDataPipe for applying custom async functions
    An asynchronous iterable-style DataPipe for applying a custom asynchronous function over an asynchronous iterable! Uses asyncio.TaskGroup from Python 3.11+ to run several tasks concurrently. Included a doctest, added a new section in the API docs under 'Mapping DataPipes', and set show_toc_level to 3 to make it show in the right sidebar.

---
+++ bambooflow/datapipes/callable.py
@@ -0,0 +1,69 @@
+"""
+Asynchronous Iterable DataPipes for asynchronous functions.
+"""
+import asyncio
+from collections.abc import AsyncIterator, Callable, Coroutine
+from typing import Any
+
+from bambooflow.datapipes.aiter import AsyncIterDataPipe
+
+
+class MapperAsyncIterDataPipe(AsyncIterDataPipe):
+    """
+    Applies an asynchronous function over each item from the source DataPipe.
+
+    Parameters
+    ----------
+    datapipe : AsyncIterDataPipe
+        The source asynchronous iterable-style DataPipe.
+    fn : Callable
+        Asynchronous function to be applied over each item.
+
+    Yields
+    ------
+    awaitable : collections.abc.Awaitable
+        An :py-term:`awaitable` object from the
+        :py-term:`asynchronous iterator <asynchronous-iterator>`.
+
+    Example
+    -------
+    >>> import asyncio
+    >>> from bambooflow.datapipes import AsyncIterableWrapper, Mapper
+    ...
+    >>> # Apply an asynchronous multiply by two function
+    >>> async def times_two(x) -> float:
+    ...     await asyncio.sleep(delay=x)
+    ...     return x * 2
+    >>> dp = AsyncIterableWrapper(iterable=[0.1, 0.2, 0.3])
+    >>> dp_map = Mapper(datapipe=dp, fn=times_two)
+    ...
+    >>> # Loop or iterate over the DataPipe stream
+    >>> it = aiter(dp_map)
+    >>> number = anext(it)
+    >>> asyncio.run(number)
+    0.2
+    >>> number = anext(it)
+    >>> asyncio.run(number)
+    0.4
+    >>> # Or if running in an interactive REPL with top-level `await` support
+    >>> number = anext(it)
+    >>> await number  # doctest: +SKIP
+    0.6
+    """
+
+    def __init__(
+        self, datapipe: AsyncIterDataPipe, fn: Callable[..., Coroutine[Any, Any, Any]]
+    ):
+        super().__init__()
+        self._datapipe = datapipe
+        self._fn = fn
+
+    async def __aiter__(self) -> AsyncIterator:
+        async with asyncio.TaskGroup() as task_group:
+            tasks: list[asyncio.Task] = [
+                task_group.create_task(coro=self._fn(data))
+                async for data in self._datapipe
+            ]
+        for task in tasks:
+            result = await task
+            yield result
